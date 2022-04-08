14a13f2f268d3a21e85810085a759e449f2cbda1
CristoforosChoras <christopher.choras@yahoo.com>
2022-04-08 09:30:44 +0200

    08.04

---
+++ 08.04/task01.js
@@ -0,0 +1,19 @@
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog, NASM and COBOL) if it is possible and false (0 in Prolog, NASM and COBOL) if not. The input values are always positive.
+
+// pump 50 miles away
+// car 25 mile per galon 
+// only 2 galons left
+// am i gonna make it to the pump
+// return
+
+
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    let calculator = mpg * fuelLeft;
+    if(calculator === distanceToPump){
+        return true;
+    }else{
+        return false;
+    }
+  };
+console.log(zeroFuel(55,25,2))
