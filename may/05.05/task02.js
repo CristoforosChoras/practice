d7efe0a9d8fc4de19d7b66324cb9d7d56520ca30
CristoforosChoras <christopher.choras@yahoo.com>
2022-05-05 18:38:12 +0300

    task 2

--- may/05.05/task02.js
+++ may/05.05/task02.js
@@ -1,9 +1,10 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 
-
-
-
 function removeExclamationMarks(s) {
-    return '';
+    return s.replace(/!/g,"");
   }
+
+
+
+  console.log(removeExclamationMarks("hello! world!"));
