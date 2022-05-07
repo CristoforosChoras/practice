1afd7690c8f835b35eb0195ad6120b20581494ad
CristoforosChoras <christopher.choras@yahoo.com>
2022-05-07 19:49:51 +0300

    task0

--- may/07.05/task07.js
+++ may/07.05/task07.js
@@ -30,5 +30,19 @@ function queueTime(customers, n) {
     }
     return Math.max(...w);
   }
+// codewars solution 
+//   function queueTime(customers, registers) {
+//     let arr = [];
+  
+//     for(let i = 0; i < registers; i++)
+//       arr[i] = 0;
+  
+//     for(let i = 0; i < customers.length; i++) {
+//       arr[0] += customers[i];
+//       arr.sort((a, b) => a - b);
+//     }
+  
+//     return arr[arr.length - 1];
+//   }
 
 console.log(queueTime([10,2,3,3], 2));
